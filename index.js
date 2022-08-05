e70d3bbad21f808d432fe186986cf31b114dc4c3
margaret <margaret.kenda@yahoo.com>
2022-08-05 14:45:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const writeCards = (array,surprise)=>{
+    let emptyArray =[]
+
+    for (item of array){
+         emptyArray.push(`Thank you, ${item}, for the wonderful ${surprise} gift!`) 
+    }
+    return emptyArray
+
+}
+
+const countDown = (number)=>{
+    for (let i = number;i>=0;i--){
+        console.log(i)
+    }
+}
